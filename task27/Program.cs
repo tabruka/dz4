2bfecd6fcfc35fb9d5449a0b04600d5006428da5
Aleksandr <tabruka@yandex.ru>
2022-10-24 13:50:11 +0300

    Добавил задачу №27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр введенного числа {number} = {SumDigits(number)}");
+
+int SumDigits(int number)
+{
+    int result = 0;
+    for (int i = 0; i <= result; i++)
+    {
+        int findlastdigit = number % 10;
+        number = number / 10;
+        result = result + findlastdigit;
+    }
+    return result;
+}
