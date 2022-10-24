58236b4e1b6501877cda5862ccaee54f7e07bc17
Aleksandr <tabruka@yandex.ru>
2022-10-24 13:50:25 +0300

    Добавил задачу №29

---
+++ task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Введите цифру 8");
+int аrray = int.Parse(Console.ReadLine());
+if(аrray == 8)
+{
+Console.Write("Массив из 8-ми элементов: ");
+}
+else
+{
+    Console.WriteLine("Введена не верная цифра, необходимо было ввести цифру 8. Попробуйте еще раз");
+}
+RandomArray(аrray);
+int[] RandomArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n && n == 8; i++)
+    {
+        array[i] = new Random().Next(0,9);
+        Console.Write($"{array[i]},");
+    }
+    return array;
+}
